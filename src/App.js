60b56a0ef313b62e59274c76b844a5be635a5e36
Raphaël Pinel <Raphael.Pinel@etteplanmore.com>
2019-06-13 10:46:25 +0300

    delete default create-react-app component and logo

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React from 'react';
+import { ThemeProvider } from 'styled-components';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+import Button from './components/Button';
+import Wrapper from './components/Wrapper';
+import HeaderText from './components/HeaderText';
 
-export default App;
+const theme = {
+  font: 'Calibri',
+};
+
+export default () => (
+  <ThemeProvider theme={theme}>
+    <Wrapper>
+      <HeaderText>I am some header text!</HeaderText>
+      <Button primary>Primary</Button>
+      <Button>Regular</Button>
+      <HeaderText>I am more header text!</HeaderText>
+    </Wrapper>
+  </ThemeProvider>
+);
